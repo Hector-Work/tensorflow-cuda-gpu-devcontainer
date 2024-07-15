6b2e2254b943f9e00f9692a9a01d95c211927bbf
hectorrobe <hector.robe@morphsites.com>
2024-07-15 16:43:02 +0100

    Simplified set up

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -1,8 +1,3 @@
-# update system
-apt update
-apt upgrade -y
-# install Linux tools and Python 3 libs
-apt install -y software-properties-common python3-dev python3-pip python3-wheel python3-setuptools
 # install Python packages
-python3 -m pip install --upgrade pip
-pip3 install -r .devcontainer/requirements.txt --user --extra-index-url https://pypi.nvidia.com
+pip install --upgrade --disable-pip-version-check pip
+pip install --disable-pip-version-check -r .devcontainer/requirements.txt
