915bac82073bf8d9afe17e9ae1a002b75efa4307
hectorrobe <hector.robe@morphsites.com>
2024-07-16 08:42:17 +0100

    Add coder server

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -1,3 +1,6 @@
+# install coder server
+curl -fsSL https://code-server.dev/install.sh | sh
+
 # install Python packages
 pip install --upgrade --disable-pip-version-check pip
 pip install --disable-pip-version-check -r .devcontainer/requirements.txt
