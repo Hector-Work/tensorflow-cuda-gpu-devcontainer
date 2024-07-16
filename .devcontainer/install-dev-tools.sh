f96e6bf70afe04327ae5903d0f7625ebf175c0a0
hectorrobe <hector.robe@morphsites.com>
2024-07-16 08:52:35 +0100

    Removed coder server as it is installed by the template

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -1,6 +1,3 @@
-# install coder server
-curl -fsSL https://code-server.dev/install.sh | sh
-
 # install Python packages
 pip install --upgrade --disable-pip-version-check pip
 pip install --disable-pip-version-check -r .devcontainer/requirements.txt
